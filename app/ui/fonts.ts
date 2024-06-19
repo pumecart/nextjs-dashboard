3725766c4184b5341facd5c233e2f59774b39c16
Pumehana Cartagena <pum3hanacarta@byui.edu>
2024-06-18 23:07:29 -0400

    program complete

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana} from 'next/font/google';
+
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusi = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700']
+});
