16560c2ec643193d273f37a73208b4c26ef12b15
Pumehana Cartagena <pum3hanacarta@byui.edu>
2024-06-29 16:22:28 -0400

    update dependencies

--- app/auth.config.ts
+++ app/auth.config.ts
@@ -4,6 +4,7 @@ export const authConfig = {
   pages: {
     signIn: '/login',
   },
+  providers: [], // Add providers with an empty array for now
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
@@ -17,5 +18,4 @@ export const authConfig = {
       return true;
     },
   },
-  providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
